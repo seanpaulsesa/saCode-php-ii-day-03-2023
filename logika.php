9ba1d4f5e4e3a6754ed174ef041f6edd89bf1779
seanpaulsesa <pauluspaul7700@gmail.com>
2023-03-21 18:35:25 +0900

    menambahkan logika.php

---
+++ logika.php
@@ -0,0 +1,16 @@
+<?php
+
+    //penulisan array yanh lama
+    $namasiswa = array("Elia", "grace","melpen","jekson","delvi","obed");
+
+    //penulisan array yang baru 
+    $nama = ["elia","grace","melpen"];
+
+    // var_dump($namasiswa[0]);
+
+    echo "<br>", $nama[0];
+    echo "<br>", $nama[1];
+    echo "<br>", $nama[2];
+
+    foreach
+?>
---
+++ tampilkandata.php
@@ -0,0 +1,28 @@
+<?php
+
+    $namasiswa = ['elia','obet','aldhi','delvi'];
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tampilkan data dari array</title>
+</head>
+<body>
+    <h3>Data Siswa</h3>
+    <table>
+        <tr>
+            <td>No</td>
+            <td>Nama</td>
+        </tr>
+        <tr>
+        <td>1</td>
+        <td>elia</td>
+        </tr>
+    </table>
+</body>
+</html>
